187d3a69abf2c24d140f1d7cce4dbddeb2af1c34
Pedram <pedram@hermes.dev>
2026-08-02 17:06:11 +0330

    fix: SessionListScreen.kt - add both ExperimentalFoundationApi and ExperimentalMaterial3Api
    - combinedClickable needs ExperimentalFoundationApi
    - PullToRefreshBox needs ExperimentalMaterial3Api
    - Both annotations now on SessionListBody

--- app/src/main/kotlin/com/peditx/android/sessions/SessionListScreen.kt
+++ app/src/main/kotlin/com/peditx/android/sessions/SessionListScreen.kt
@@ -275,7 +275,7 @@ fun SessionListScreen(
  * adaptive two-pane shell lands. Nav items now live in the slide-out drawer owned by
  * [SessionListScreen].
  */
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun SessionListBody(
     viewModel: SessionListViewModel,
