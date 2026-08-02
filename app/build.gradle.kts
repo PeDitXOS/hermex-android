8cf5b5cb338f72558e9441166fc05d2cf4ecb744
Pedram <pedram@hermes.dev>
2026-08-02 08:04:32 +0330

    chore: bump version to 1.0.9 / 109 - force clean build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.peditx.hermex"
         minSdk = 26
         targetSdk = 36
-        versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 108
-                versionName = "1.0.8"
+        versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 109
+                versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
