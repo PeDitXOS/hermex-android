4dd05828104e431dcd57c4e231672e310bd4b22c
Pedram <pedram@hermes.dev>
2026-08-02 14:58:02 +0330

    chore: bump version to 1.0.14 / 114

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.peditx.hermex"
         minSdk = 26
         targetSdk = 36
-        versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 113
-                versionName = "1.0.13"
+        versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 114
+                versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
